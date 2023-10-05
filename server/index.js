4d14df5e0b806970c0ca38677d33552aa5fe7315
Ednardo Gomes <ednardo@gomes.com>
2023-10-05 12:15:32 -0300

    Conectando o frontend com o backend.

--- server/index.js
+++ server/index.js
@@ -1,17 +1,25 @@
 import express from 'express'
 import cors from 'cors'
 import { download } from './download.js'
+import { transcribe } from './transcribe.js'
+import { summarize } from './summarize.js'
 
 const serve = express()
-
+serve.use(express.json())
 serve.use(cors())
 
-serve.get('/summary/:id', (request ,response ) => {
-  const videoId = request.params.id
+serve.get('/summary/:id', async (request ,response ) => {
+  await download(request.params.id)
+
+  const result = await transcribe()
   
-  download(videoId)
-  
-  response.json({ result: "Download realizado com sucesso." })
+  return response.json({ result })
+})
+
+serve.post('/summary', async (request, response) => {
+  const result = await summarize(request.body.text)
+
+  return response.json({ result })
 })
 
 serve.listen(3333, () => console.log('Server is runnig on port 3333'))
