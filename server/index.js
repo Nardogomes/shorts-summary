adff6ef572b0764ff932237f77a4f8937229aa7f
Ednardo Gomes <ednardo@gomes.com>
2023-10-06 12:07:29 -0300

    Implementando uso de inteligência artifical.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
-import express from 'express'
 import cors from 'cors'
+import express from 'express'
+
+import { convert } from './convert.js'
 import { download } from './download.js'
 import { transcribe } from './transcribe.js'
 import { summarize } from './summarize.js'
@@ -9,17 +11,26 @@ serve.use(express.json())
 serve.use(cors())
 
 serve.get('/summary/:id', async (request ,response ) => {
-  await download(request.params.id)
-
-  const result = await transcribe()
-  
-  return response.json({ result })
+  try {
+    await download(request.params.id)
+    const audioConverted = await convert()
+    const result = await transcribe(audioConverted)
+    
+    return response.json({ result })
+  } catch (error) {
+    console.log(error)
+    return response.json({ error })
+  }
 })
 
 serve.post('/summary', async (request, response) => {
-  const result = await summarize(request.body.text)
-
-  return response.json({ result })
+  try {
+    const result = await summarize(request.body.text)
+    return response.json({ result })
+  } catch (error) {
+    console.log(error)
+    return response.json({ error })
+  }
 })
 
 serve.listen(3333, () => console.log('Server is runnig on port 3333'))
